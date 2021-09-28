727767647b28de7423f66c4526a013c0d57bc20b
Yevhenii Klimov <24evgeniy03@gmail.com>
2021-09-28 19:31:43 +0000

    Update Company.cs

--- Inheritance/Company.cs
+++ Inheritance/Company.cs
@@ -1,17 +1,43 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 
 namespace InheritanceTask
 {
-    //TODO: Create public class 'Company' here
+    public class Company
+    {
+        private Employee[] employees;
 
-    //TODO: Define private field that is array of employees: 'employees'
-
-    //TODO: Define constructor that gets array of employees, and assign them to its field
-
-    //TODO: Define public method 'GiveEverbodyBonus' with parameter 'companyBonus', that set basic bonus to every employee in the company
-    //TODO: Define public method 'TotalToPay', that returns the total salary + bonus of all employees of the company 
-    //TODO: Define public method 'NameMaxSalary', that returns emloyee’s name who has maximum salary + bonus in the company 
+        public Company(Employee[] employees)
+        {
+            this.employees = employees;
+        }
+        public void GiveEverybodyBonus (decimal companyBonus)
+        {
+            foreach (Employee employee in employees)
+                employee.SetBonus(companyBonus);
+        }
+        public decimal TotalToPay()
+        {
+            decimal total = 0;
+            foreach (Employee employee in employees)
+                total += employee.ToPay();
+            return total;
+        }
+        public string NameMaxSalary()
+        {
+            decimal maximum = employees[0].ToPay();
+            Employee employee = employees[0];
+            for(int i = 0; i < employees.Length; i++)
+            {
+                if (employees[i].ToPay() > maximum)
+                {
+                    maximum = employees[i].ToPay();
+                    employee = employees[i];
+                }
+            }
+            return employee.Name;
+        }
+    }
 }
